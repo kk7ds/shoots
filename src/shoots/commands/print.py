746344a4af2448c80c5832a0c990f476318633cd
Dan Smith <dsmith@danplanet.com>
2024-02-13 20:30:44 -0800

    Provide upload-and-print function

--- src/shoots/commands/print.py
+++ src/shoots/commands/print.py
@@ -1,8 +1,11 @@
 import argparse
 import logging
+import os
 
 from shoots import cli
 from shoots import printer
+
+LOG = logging.getLogger(__name__)
 
 
 class Print(cli.ShootsCommand):
@@ -17,9 +20,20 @@ class Print(cli.ShootsCommand):
                        help='Do not flow calibrate')
         p.add_argument('--timelapse', action='store_true', default=False,
                        help='Record timelapse')
+        p.add_argument('--upload', help='Upload this file and then print it')
 
-    def execute(self, args:argparse.Namespace, p: printer.Printer):
-        p.print(file=args.file,
+    def execute(self, args: argparse.Namespace, p: printer.Printer):
+        if args.upload:
+            remote_file = os.path.basename(args.file)
+            ftp = p.connect_ftp()
+            LOG.info('Uploading %s to %s', args.file, remote_file)
+            with open(args.file, 'rb') as f:
+                ftp.storbinary('STOR %s' % remote_file, f.read)
+            LOG.info('Uploaded')
+        else:
+            remote_file = args.file
+
+        p.print(file=remote_file,
                 use_ams=not args.no_ams,
                 bed_leveling=not args.no_level,
                 flow_cali=not args.no_flowcal,
