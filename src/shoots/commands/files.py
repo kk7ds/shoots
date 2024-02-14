f2984789570b7f685c5145ddaa97ff7b0eaccf83
Dan Smith <dsmith@danplanet.com>
2024-02-13 20:22:19 -0800

    Move ftp connection to printer

--- src/shoots/commands/files.py
+++ src/shoots/commands/files.py
@@ -2,7 +2,6 @@ import argparse
 import ftplib
 import logging
 import os
-import ssl
 
 from shoots import cli
 from shoots import printer
@@ -10,57 +9,11 @@ from shoots import printer
 LOG = logging.getLogger(__name__)
 
 
-class ImplicitFTP_TLS(ftplib.FTP_TLS):
-    """FTP_TLS subclass to support implicit FTPS.
-    Constructor takes a boolean parameter ignore_PASV_host whether o ignore
-    the hostname in the PASV response, and use the hostname from the session
-    instead
-    """
-    def __init__(self, *args, **kwargs):
-        self.ignore_PASV_host = kwargs.get('ignore_PASV_host') == True
-        super().__init__(*args, {k: v for k, v in kwargs.items()
-                                 if not k == 'ignore_PASV_host'})
-        self._sock = None
-
-    @property
-    def sock(self):
-        """Return the socket."""
-        return self._sock
-
-    @sock.setter
-    def sock(self, value):
-        """When modifying the socket, ensure that it is ssl wrapped."""
-        if value is not None and not isinstance(value, ssl.SSLSocket):
-            value = self.context.wrap_socket(value)
-        self._sock = value
-
-    def ntransfercmd(self, cmd, rest=None):
-        """Override the ntransfercmd method"""
-        conn, size = ftplib.FTP.ntransfercmd(self, cmd, rest)
-        conn = self.sock.context.wrap_socket(
-            conn, server_hostname=self.host, session=self.sock.session
-        )
-        return conn, size
-
-    def makepasv(self):
-        host, port = super().makepasv()
-        return (self.host if self.ignore_PASV_host else host), port
-
-
 class Files(cli.ShootsCommand):
     def add_args(self, subparsers: argparse._SubParsersAction):
         p = subparsers.add_parser('files', help='Manage files on printer')
         p.add_argument('subcommand', choices=['list', 'remove', 'get'])
         p.add_argument('--file', help='File or directory to act on')
-
-    def _connect(self, args, p):
-        self._ftp = ImplicitFTP_TLS()
-        LOG.debug('Connecting FTP to %s' % p.host)
-        self._ftp.connect(p.host, port=990)
-        LOG.debug('Logging into FTP with %s' % p.key)
-        self._ftp.login('bblp', p.key)
-        LOG.debug('Starting secure session')
-        self._ftp.prot_p()
 
     def list(self, args: argparse.Namespace, p: printer.Printer):
         if args.file:
@@ -101,7 +54,7 @@ class Files(cli.ShootsCommand):
         print('Fetched %s' % localfn)
 
     def execute(self, args: argparse.Namespace, p: printer.Printer):
-        self._connect(args, p)
+        self._ftp = p.connect_ftp()
         if args.subcommand == 'list':
             return self.list(args, p)
         elif args.subcommand == 'remove':
--- src/shoots/printer.py
+++ src/shoots/printer.py
@@ -1,8 +1,8 @@
 import datetime
 import json
+import ftplib
 import logging
 import pprint
-import socket
 import ssl
 import threading
 
@@ -25,6 +25,43 @@ class NotReady(Exception):
     pass
 
 
+class ImplicitFTP_TLS(ftplib.FTP_TLS):
+    """FTP_TLS subclass to support implicit FTPS.
+    Constructor takes a boolean parameter ignore_PASV_host whether o ignore
+    the hostname in the PASV response, and use the hostname from the session
+    instead
+    """
+    def __init__(self, *args, **kwargs):
+        self.ignore_PASV_host = kwargs.get('ignore_PASV_host') == True
+        super().__init__(*args, {k: v for k, v in kwargs.items()
+                                 if not k == 'ignore_PASV_host'})
+        self._sock = None
+
+    @property
+    def sock(self):
+        """Return the socket."""
+        return self._sock
+
+    @sock.setter
+    def sock(self, value):
+        """When modifying the socket, ensure that it is ssl wrapped."""
+        if value is not None and not isinstance(value, ssl.SSLSocket):
+            value = self.context.wrap_socket(value)
+        self._sock = value
+
+    def ntransfercmd(self, cmd, rest=None):
+        """Override the ntransfercmd method"""
+        conn, size = ftplib.FTP.ntransfercmd(self, cmd, rest)
+        conn = self.sock.context.wrap_socket(
+            conn, server_hostname=self.host, session=self.sock.session
+        )
+        return conn, size
+
+    def makepasv(self):
+        host, port = super().makepasv()
+        return (self.host if self.ignore_PASV_host else host), port
+
+
 class Printer:
     def __init__(self, host, key, device, reconnect=False):
         self._host = host
@@ -38,6 +75,7 @@ class Printer:
             self.log = LOG.getChild(self._device)
         else:
             self.log = LOG
+        self._ftp = None
 
         self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
         self.client.enable_logger()
@@ -246,3 +284,14 @@ class Printer:
 
         with self._condition:
             self._condition.notify()
+
+    def connect_ftp(self):
+        if not self._ftp:
+            self._ftp = ImplicitFTP_TLS()
+            self.log.debug('Connecting FTP to %s' % self.host)
+            self._ftp.connect(self.host, port=990)
+            self.log.debug('Logging into FTP with %s' % self.key)
+            self._ftp.login('bblp', self.key)
+            self.log.debug('Starting secure session')
+            self._ftp.prot_p()
+        return self._ftp
