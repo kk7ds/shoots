4dbb49a6d47cee7f18245fcfb705e80e03012d15
Dan Smith <dsmith@danplanet.com>
2024-02-13 18:19:03 -0800

    Add file download

--- src/shoots/commands/files.py
+++ src/shoots/commands/files.py
@@ -1,6 +1,7 @@
 import argparse
 import ftplib
 import logging
+import os
 import ssl
 
 from shoots import cli
@@ -49,8 +50,8 @@ class ImplicitFTP_TLS(ftplib.FTP_TLS):
 class Files(cli.ShootsCommand):
     def add_args(self, subparsers: argparse._SubParsersAction):
         p = subparsers.add_parser('files', help='Manage files on printer')
-        p.add_argument('subcommand', choices=['list', 'remove'])
-        p.add_argument('--file', help='File to act on')
+        p.add_argument('subcommand', choices=['list', 'remove', 'get'])
+        p.add_argument('--file', help='File or directory to act on')
 
     def _connect(self, args, p):
         self._ftp = ImplicitFTP_TLS()
@@ -62,13 +63,27 @@ class Files(cli.ShootsCommand):
         self._ftp.prot_p()
 
     def list(self, args: argparse.Namespace, p: printer.Printer):
+        if args.file:
+            LOG.debug('Changing directory')
+            self._ftp.cwd(args.file)
         LOG.debug('Listing')
         files = self._ftp.nlst()
         ignore = ['cache', 'ipcam', 'timelapse']
         for fn in files:
+            try:
+                sz = self._ftp.size(fn)
+            except ftplib.error_perm:
+                sz = 0
+            units = 'B'
+            if sz > 1024:
+                sz /= 1024
+                units = 'KiB'
+            if sz > 1024:
+                sz /= 1024
+                units = 'MiB'
             if fn in ignore:
                 continue
-            print(fn)
+            print('%4i%-3s: %s' % (sz, units, fn))
 
     def remove(self, args: argparse.Namespace, p: printer.Printer):
         if not args.file:
@@ -76,11 +91,22 @@ class Files(cli.ShootsCommand):
             return 1
         self._ftp.delete(args.file)
 
+    def get(self, args: argparse.Namespace, p: printer.Printer):
+        localfn = os.path.basename(args.file)
+        if os.path.exists(localfn):
+            print('File %r already exists, not overwriting' % args.file)
+            return 1
+        with open(localfn, 'wb') as f:
+            self._ftp.retrbinary('RETR %s' % args.file, f.write)
+        print('Fetched %s' % localfn)
+
     def execute(self, args: argparse.Namespace, p: printer.Printer):
         self._connect(args, p)
         if args.subcommand == 'list':
             return self.list(args, p)
         elif args.subcommand == 'remove':
             return self.remove(args, p)
+        elif args.subcommand == 'get':
+            return self.get(args, p)
         else:
             raise RuntimeError('Unknown command %s' % args.subcommand)
